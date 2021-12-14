ccf6bfe4d361be5479fdafca437f563cf9a585c9
Mudroff <mudrsemen@ya.ru>
2021-12-14 12:50:02 +0300

    fix for 6th task

--- урок_3/Задание_6.py
+++ урок_3/Задание_6.py
@@ -1,9 +1,21 @@
 num = int(input())
 if num == 0:
     print('Green')
-elif num >= 1 and num <= 10 or num >= 19 and num <= 28:
+elif num >= 1 and num <= 10 and num % 2 == 0:
     print('black')
-elif num >= 11 and num <= 18 or num >= 29 and num <= 36:
+elif num >= 1 and num <= 10 and num % 2 != 0:
+    print('red')
+elif num >= 19 and num <= 28 and num % 2 == 0:
+    print('black')
+elif num >= 19 and num <= 28 and num % 2 != 0:
+    print('red')
+elif num >= 11 and num <= 18 and num % 2 == 0:
+    print('black')
+elif num >= 11 and num <= 18 and num % 2 != 0:
+    print('red')
+elif num >= 29 and num <= 36 and num % 2 == 0:
+    print('black')
+elif num >= 29 and num <= 36 and num % 2 != 0:
     print('red')
 elif num < 0 or num > 36:
     print('Error')
