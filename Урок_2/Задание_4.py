2840ffb6fcaf25f53678d7a9b98d403cbba7accb
Mudroff <mudrsemen@ya.ru>
2021-12-16 15:50:52 +0300

    fix for 4th lesson

--- Урок_2/Задание_4.py
+++ Урок_2/Задание_4.py
@@ -1,10 +1,13 @@
 
+from math import floor
+
+
 r = int(input())
 k = int(input())
 n = int(input())
 kop = k/100
 price = r+kop
 sum = price*n
-sum_kop = sum*100 % 100
-print('За', n, 'мячей', 'надо заплатить', int(
-    sum), 'рублей и', int(sum_kop), 'копеек')
+sum_kop = round(sum*100) % 100
+print('За', n, 'мячей', 'надо заплатить', floor(
+    sum), 'рублей и', sum_kop, 'копеек')
