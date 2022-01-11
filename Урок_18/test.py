3b31f88d41a4fac72a18a021a8aecf617910778b
Mudroff <mudrsemen@ya.ru>
2022-01-11 10:04:06 +0300

    lesson 18

--- Урок_18/test.py
+++ Урок_18/test.py
@@ -13,25 +13,6 @@ def valid_answer(answer):
     return answer
 
 
-questions = [
-    'вопрос 1',
-    'вопрос 2',
-    'вопрос 3',
-    'вопрос 4',
-    'вопрос 5'
-]
-
-answers = [1, 2, 3, 4, 5]
-
-results = [
-    'идиот',
-    'кретин',
-    'дурак',
-    'нормальный',
-    'талант',
-    'гений',
-]
-
 right_answers_ammount = 0
 
 
@@ -41,18 +22,43 @@ def count_right_answers(user_answer, right_answer, right_answers_ammount):
     return right_answers_ammount
 
 
-def randomize_questions(questions):
-    random_index = randint(0, len(questions))
-    question = questions.pop([random_index])
+def randomize_question_index(questions):
+    random_index = randint(0, len(questions)-1)
+    return random_index
+
+
+def randomize_questions(questions, random_index):
+    question = questions.pop(random_index)
     return question
 
 
 while True:
+    questions = [
+        'вопрос 1',
+        'вопрос 2',
+        'вопрос 3',
+        'вопрос 4',
+        'вопрос 5'
+    ]
+
+    answers = [1, 2, 3, 4, 5]
+
+    results = [
+        'идиот',
+        'кретин',
+        'дурак',
+        'нормальный',
+        'талант',
+        'гений',
+    ]
+
     user_name = input('Введите Ваше имя:')
     for i in range(len(questions)):
-        print(i+1, randomize_questions(questions))
+        random_index = randomize_question_index(questions)
+        question = randomize_questions(questions, random_index)
+        print(i+1, question)
         user_answer = int(input())
-        right_answer = answers[i]
+        right_answer = answers.pop(random_index)
         right_answers_ammount = count_right_answers(
             user_answer, right_answer, right_answers_ammount)
     print('Правильных ответов:', right_answers_ammount)
