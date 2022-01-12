0c53074563e36454774ccf5451aa604c041cb8d4
Mudroff <mudrsemen@ya.ru>
2022-01-12 16:40:52 +0300

    lesson 19

--- Урок_18/test.py
+++ Урок_18/test.py
@@ -1,8 +1,32 @@
 from random import randint
-import random
+from pathlib import Path
 
 
-def valid_answer(answer):
+def get_questions():
+
+    questions = [
+        'вопрос 1',
+        'вопрос 2',
+        'вопрос 3',
+        'вопрос 4',
+        'вопрос 5',
+        'вопрос 6',
+        'вопрос 7',
+        'вопрос 8',
+        'вопрос 9',
+        'вопрос 10',
+        'вопрос 11',
+        'вопрос 12'
+    ]
+    return questions
+
+
+def get_answers():
+    answers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    return answers
+
+
+def valid_answer_end(answer):
     while True:
         if answer == '+' or answer == '-':
             break
@@ -13,13 +37,16 @@ def valid_answer(answer):
     return answer
 
 
-right_answers_ammount = 0
-
-
-def count_right_answers(user_answer, right_answer, right_answers_ammount):
-    if user_answer == right_answer:
-        right_answers_ammount += 1
-    return right_answers_ammount
+def valid_answer_digit(answer):
+    while True:
+        if answer.isdigit():
+            answer = int(answer)
+            break
+        else:
+            print('Ответ должен состоять из цифр')
+            answer = input()
+            continue
+    return answer
 
 
 def randomize_question_index(questions):
@@ -32,24 +59,38 @@ def randomize_questions(questions, random_index):
     return question
 
 
-while True:
-    questions = [
-        'вопрос 1',
-        'вопрос 2',
-        'вопрос 3',
-        'вопрос 4',
-        'вопрос 5'
-    ]
+def calc_results(questions_amount, right_answers_ammount):
+    right_answers_percent = 100/questions_amount*right_answers_ammount
+    if right_answers_percent == 0:
+        return results[0]
+    elif right_answers_percent > 0 and right_answers_percent <= 20:
+        return results[1]
+    elif right_answers_percent > 20 and right_answers_percent <= 40:
+        return results[2]
+    elif right_answers_percent > 40 and right_answers_percent <= 60:
+        return results[3]
+    elif right_answers_percent > 60 and right_answers_percent <= 80:
+        return results[4]
+    elif right_answers_percent > 80 and right_answers_percent <= 100:
+        return results[5]
 
-    answers = [1, 2, 3, 4, 5]
+
+file = open('Урок_18/info.txt', 'a')
+while True:
+    # path = Path('info.txt')
+
+    questions = get_questions()
+    answers = get_answers()
+    right_answers_ammount = 0
+    qeutions_ammount = len(questions)
 
     results = [
-        'идиот',
-        'кретин',
-        'дурак',
-        'нормальный',
-        'талант',
-        'гений',
+        'идиот \n',
+        'кретин \n',
+        'дурак \n',
+        'нормальный \n',
+        'талант \n',
+        'гений \n',
     ]
 
     user_name = input('Введите Ваше имя:')
@@ -57,18 +98,29 @@ while True:
         random_index = randomize_question_index(questions)
         question = randomize_questions(questions, random_index)
         print(i+1, question)
-        user_answer = int(input())
-        right_answer = answers.pop(random_index)
-        right_answers_ammount = count_right_answers(
-            user_answer, right_answer, right_answers_ammount)
+        user_answer = input()
+        user_answer = valid_answer_digit(user_answer)
+        right_answer = answers[random_index]
+        if user_answer == right_answer:
+            right_answers_ammount += 1
+        answers.pop(random_index)
+    result = calc_results(qeutions_ammount, right_answers_ammount)
+    resume = [user_name, right_answers_ammount, result, ]
+    resume = " ".join(map(str, resume))
+    print(resume)
+    file.write(resume)
     print('Правильных ответов:', right_answers_ammount)
-    print(user_name, ',Вы -', results[right_answers_ammount])
+    print(user_name, ',Вы -', result)
     print('Если хотите сыграть еще раз введите +, если не хотите введите -')
     answer = input()
-    answer = valid_answer(answer)
+    answer = valid_answer_end(answer)
     if answer == '+':
         right_answers_ammount = 0
         continue
     else:
         print('Спасибо за участие')
+        file.close()
         break
+print(f'имя', 'Кол-во правильных ответов', 'Результат')
+file = open('Урок_18/info.txt', 'r')
+print(file.read())
