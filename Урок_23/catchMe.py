34917a2f64139c52f5055416a538dcc047e1932e
Mudroff <mudrsemen@ya.ru>
2022-01-14 15:21:41 +0300

    CathMe game

---
+++ Урок_23/catchMe.py
@@ -0,0 +1,142 @@
+from cgitb import text
+from dis import dis
+from turtle import width
+import pygame
+import random
+import time
+import math
+
+
+class Ball:
+    def __init__(self, display):
+        self.display = display
+        self.color = pygame.Color('red')
+        self.center_x = 100
+        self.center_y = 100
+        self.radius = 30
+
+        self.vx = 2
+        self.vy = 2
+
+    def show(self):
+        pygame.draw.circle(self.display, self.color,
+                           (self.center_x, self.center_y), self.radius)
+
+    def go(self):
+
+        self.center_x += self.vx
+        self.center_y += self.vy
+
+    def clear(self):
+        pygame.draw.circle(self.display, pygame.Color(
+            'white'), (self.center_x, self.center_y), self.radius)
+
+    def move(self):
+        self.clear()
+        self.go()
+        self.show()
+
+    def stop(self):
+        self.vx = 0
+        self.vy = 0
+
+    def get_coords(self):
+        x = self.center_x
+        y = self.center_y
+        return x, y
+
+    def show_speed(self):
+        return self.vx, self.vy
+
+
+class RandomPointBall(Ball):
+    def __init__(self, display):
+        super().__init__(display)
+        self.color = pygame.Color('Green')
+
+        width, height = display.get_size()
+        self.center_x = random.randint(self.radius, width - self.radius)
+        self.center_y = random.randint(self.radius, height - self.radius)
+
+
+class PointBall(Ball):
+    def __init__(self, display, x, y):
+        super().__init__(display)
+        self.color = pygame.Color('yellow')
+        self.center_x = x
+        self.center_y = y
+
+
+class RandomPointMovableBall(RandomPointBall):
+    def __init__(self, display):
+        super().__init__(display)
+        speed_array = [-3, -2, -1, 1, 2, 3]
+        self.vx = speed_array[random.randint(0, len(speed_array)-1)]
+        self.vy = speed_array[random.randint(0, len(speed_array)-1)]
+
+
+def show_text(text):
+    font = pygame.font.Font(None, 48)
+    text = font.render(text, True,  (0, 128, 0))
+    text_field = text.get_rect()
+    text_field.center = (30, 30)
+    display.blit(text, text_field)
+
+
+def is_in_circle(mouse_x, mouse_y, ball_x, ball_y, ball_radius):
+
+    if (mouse_x - ball_x)**2 / ball_radius**2 + (mouse_y - ball_y)**2 / ball_radius**2 < 1:
+        return True
+    else:
+        return False
+
+
+def is_in_field(ball_coord_x, ball_coord_y, width, height):
+    if ball_coord_x > width - 30 or ball_coord_x < 30 or ball_coord_y > height - 30 or ball_coord_y < 30:
+        return False
+    else:
+        return True
+
+
+pygame.init()
+
+width = 1000
+height = 800
+display = pygame.display.set_mode((width, height))
+display.fill(pygame.Color('white'))
+balls_caught_counter = 0
+balls = []
+for i in range(10):
+    ball = RandomPointMovableBall(display)
+    ball.show()
+    balls.append(ball)
+pygame.display.flip()
+
+time.sleep(2)
+
+clock = pygame.time.Clock()
+while True:
+
+    event_list = pygame.event.get()
+    for event in event_list:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            display.fill(pygame.Color('white'))
+            pygame.display.flip()
+            for ball in balls:
+                mouse_position = pygame.mouse.get_pos()
+                mouse_position_x = mouse_position[0]
+                mouse_position_y = mouse_position[1]
+                ball_coords = ball.get_coords()
+                ball_coord_x = ball_coords[0]
+                ball_coord_y = ball_coords[1]
+                if is_in_circle(mouse_position_x, mouse_position_y, ball_coord_x, ball_coord_y, 30) and is_in_field(ball_coord_x, ball_coord_y, width, height):
+                    ball.stop()
+                    # if ball_coord_x > width - 30 or ball_coord_x < 30 or ball_coord_y > height - 30 or ball_coord_y < 30:
+                    balls_caught_counter += 1
+        show_text(str(balls_caught_counter))
+    for ball in balls:
+        ball.move()
+    pygame.display.flip()
+    clock.tick(60)
